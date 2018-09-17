2902353e5cded6891f91341153e04d6ecc038e02
fj02090710 <845174804@qq.com>
2018-09-17 17:28:29 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  title: "云平台操作手册",
+  description: "The description of the site.",
+  head: [["link", { rel: "icon", href: `/logo.png` }]],
+  base: "/",
+  dest: "./dist",
+
+  themeConfig: {
+    nav: [
+      { text: "云平台操作手册", link: "/" }
+    ],
+    sidebar: {
+      '/': genSidebarConfig('云平台操作手册')
+    },
+    lastUpdated: 'Last Updated'
+  },
+
+  markdown: {
+    // options for markdown-it-anchor
+    anchor: { permalink: false },
+    config: md => {
+      md.use(require("markdown-it-katex"));
+    }
+  }
+};
+
+function genSidebarConfig (title) {
+  return [
+    {
+      title,
+      collapsable: false,
+      children: [
+        '',
+        'getting-started',
+        'customize',
+        'advanced',
+      ]
+    }
+  ]
+}
+
